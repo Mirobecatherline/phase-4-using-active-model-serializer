75e72f16e9a1cad1d0be52d557ab8588394f997a
catherline nyanduko mirobe <catherline.nyanduko@student.moringaschool.com>
2022-09-30 16:19:38 +0300

    commit tree 175290a323bafb012cbc6d93d0b053dd4ed1541b

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -9,6 +9,11 @@ class MoviesController < ApplicationController
   def show
     movie = Movie.find(params[:id])
     render json: movie
+    #we can do this by using to_json-> render json: movie.to_json(only: [:id, :title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director]/render json: movie.to_json(except: [:created_at, :updated_at]))
+  end
+  def summary
+    movie = Movie.find(params[:id])
+    render json: movie, serializer: MovieSummarySerializer
   end
 
   private
